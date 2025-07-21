c4fc8611937529288ea09b5394006f3ec431af60
Stephen Sherratt <stephen@sherra.tt>
2025-07-21 16:08:18 +1000

    Add action

--- flake.nix
+++ flake.nix
@@ -17,20 +17,20 @@
         # correspond to a dynamically-linked and statically-linked instance of
         # dune, respectively.
         make-dune-pkgs = { ref, rev }: {
-          dynamic = import ./dune-versioned.nix {
+          dynamic = import ./dune.nix {
             inherit ref rev pkgs;
             static = false;
           };
-          static = import ./dune-versioned.nix {
+          static = import ./dune.nix {
             inherit ref rev pkgs;
             static = true;
           };
         };
       in {
         packages = {
-          dune_3_19_1 = make-dune-pkgs {
-            ref = "3.19.1";
-            rev = "76c0c3941798f81dcc13a305d7abb120c191f5fa";
+          dune = make-dune-pkgs {
+            ref = "main";
+            rev = "fd307dfd897c0d764bbf5b151e01f99f6be05111";
           };
         };
       });
