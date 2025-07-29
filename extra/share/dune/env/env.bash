f598c6eb5bcef728698b0dbaddcaa7386aeaf13e
Stephen Sherratt <stephen@sherra.tt>
2025-07-30 09:58:38 +1000

    Add env scripts for different shells (#3)
    The env scripts take care of setting the PATH variable to include the
    path to where dune is installed, and registering shell completions for
    dune.
    
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

---
+++ extra/share/dune/env/env.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+__dune_env() {
+  if [ "$#" != "1" ]; then
+    echo "__dune_env expected 1 argument, got $#"
+    return
+  fi
+  local ROOT="$1"
+
+  # Add dune to PATH unless its already present.
+  # Affix colons on either side of $PATH to simplify matching (based on
+  # rustup's env script).
+  case :"$PATH": in
+    *:"$ROOT/bin":*)
+      # Do nothing since the bin directory is already in PATH.
+      ;;
+    *)
+      # Prepending path in case a system-installed dune needs to be overridden
+      export PATH="$ROOT/bin:$PATH"
+      ;;
+  esac
+
+  . "$ROOT"/share/bash-completion/completions/dune
+}
