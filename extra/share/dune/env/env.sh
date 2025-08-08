5b45399f5a57278dd2c8c15f27186d18147ad0b6
Stephen Sherratt <stephen@sherra.tt>
2025-08-08 15:57:18 +1000

    Add posix sh env script
    This amounted taking the bash env script and removing its use of the
    `local` keyword and changing is extension to ".sh". The posix env script
    also doesn't source the bash completion script as it uses some
    bash-specific features.
    
    This allows the dune binary distro to be used on machines with minimal
    shells such as the ash shell in the default environment on alpine linux.
    
    Also adds a test that runs shellcheck on env.sh to make sure it stays
    posix-compatible.
    
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- extra/share/dune/env/env.sh
+++ extra/share/dune/env/env.sh
@@ -1,27 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 __dune_env() {
   if [ "$#" != "1" ]; then
     echo "__dune_env expected 1 argument, got $#"
     return
   fi
-  local ROOT="$1"
+  __dune_root="$1"
 
   # Add dune to PATH unless it's already present.
   # Affix colons on either side of $PATH to simplify matching (based on
   # rustup's env script).
   case :"$PATH": in
-    *:"$ROOT/bin":*)
+    *:"$__dune_root/bin":*)
       # Do nothing since the bin directory is already in PATH.
       ;;
     *)
       # Prepending path in case a system-installed dune needs to be overridden
-      export PATH="$ROOT/bin:$PATH"
+      export PATH="$__dune_root/bin:$PATH"
       ;;
   esac
-
-  # Load bash completions for dune.
-  # Suppress warning from shellcheck as it can't see the completion script.
-  # shellcheck disable=SC1091
-  . "$ROOT"/share/bash-completion/completions/dune
 }
