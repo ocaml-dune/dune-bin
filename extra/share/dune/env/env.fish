f3669b4a934fbdb78c4e8b8352fa9a0a1fc569eb
Stephen Sherratt <stephen@sherra.tt>
2025-08-11 21:22:10 +1000

    Fix paths in env scripts in nested shells
    When starting a shell from an existing shell where the shell config has
    already been sourced, or when sourcing a shell config for a second time
    within the same shell session, any opam setup in the shell config will
    re-insert the bin directory from the current opam switch at the
    beginning of PATH.
    
    Dune's setup logic doesn't re-insert directories already present in PATH
    as dune may share its bin directory with other programs reliant on the
    current PATH order. Thus opam's re-initialization will result in any
    dune executable in the current opam switch taking precedence over the
    binary distro.
    
    To prevent this, the __dune_env function detects when it's called
    subsequent times in a shell session, and if it succeeded the first time
    in correctly setting up the environment for the dune binary distribution
    will remove any opam directories from PATH which precede the first
    instance of location of the dune binary distro in PATH. Opam bin
    directories following the location of the binary distro in PATH, as well
    as all other directories in PATH are left unchanged.
    
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- extra/share/dune/env/env.fish
+++ extra/share/dune/env/env.fish
@@ -1,8 +1,63 @@
 #!/usr/bin/env fish
 
+if ! set -q __DUNE_SETUP_STATE
+    set --export __DUNE_SETUP_STATE "incomplete"
+end
+
 function __dune_env
-    set dune_bin_path "$argv[1]/bin"
-    if ! contains "$dune_bin_path" $PATH;
-        fish_add_path --prepend --path "$dune_bin_path"
+    if [ "$(count $argv)" != "1" ]
+        echo "__dune_env expected 1 argument, got $(count $argv)"
+    end
+
+    set --local dune_root "$argv[1]"
+    set --local dune_bin "$dune_root/bin"
+
+    switch "$__DUNE_SETUP_STATE"
+        case incomplete
+            if ! contains "$dune_bin" $PATH; and [ -d "$dune_bin" ]
+                fish_add_path --prepend --path "$dune_bin"
+            end
+            if [ "$(type -P dune)" = "$dune_bin/dune" ]
+                set --export __DUNE_SETUP_STATE "success"
+            else
+                # Despite modifying the environment, running `dune` would resolve to
+                # the wrong dune instance. This can happen if the bin directory
+                # containing our dune was already present in PATH behind the bin
+                # directory of the default opam switch.
+                # TODO Possibly print a warning/hint in this case to help users fix
+                # their environment.
+                set --export __DUNE_SETUP_STATE "failure"
+            end
+        case success
+            # This is at least the second time this function was called in the
+            # current environment (possibly by the user sourcing their shell config
+            # or nesting shell sessions), and the previous time it was called it
+            # successfully modified the environment to give precedence to our dune
+            # executable. check that our dune still has precedence, and attempt to
+            # undo any opam-specific path shenanigans that have taken place since the
+            # last time this function was called.
+            if [ "$(type -P dune)" != "$dune_bin/dune" ]
+                if contains "$dune_bin" $PATH
+                    # Remove all opam bin directories from the PATH variable
+                    # between the start of the PATH variable and the first
+                    # occurrence of the dune binary distro's bin directory.
+                    set --local PATH_maybe_fixed $(printf "%s\n" $PATH | \
+                        sed "1,\#^$dune_bin\$# { \#^$HOME/.opam#d; }")
+                    # Only commit the change if it actually fixed the problem.
+                    if [ "$(PATH=$PATH_maybe_fixed type -P dune)" = "$dune_bin/dune" ]
+                        set --export PATH $PATH_maybe_fixed
+                    else
+                        # The attempt to fix the PATH variable failed, so give up.
+                        set --export __DUNE_SETUP_STATE "failure"
+                    end
+                else
+                    # The dune binary distro is no longer in the PATH variable at
+                    # all, so give up.
+                    set --export __DUNE_SETUP_STATE "failure"
+                end
+            end
+        case failure
+            # A previous attempt at modifying the environment failed, so don't
+            # attempt further environment modifications here.
     end
 end
--- extra/share/dune/env/env.sh
+++ extra/share/dune/env/env.sh
@@ -1,22 +1,94 @@
 #!/bin/sh
 
+# Equivalent to `which dune`, but `which` might not be installed.
+__dune_which() {
+  echo "$PATH" | \
+    tr ':' '\n' |\
+    while read -r entry; do
+      if test -x "$entry/dune"; then
+        echo "$entry/dune"
+        return 0
+      fi
+    done
+    return 1
+}
+
+export __DUNE_SETUP_STATE="${__DUNE_SETUP_STATE:-incomplete}"
+
 __dune_env() {
   if [ "$#" != "1" ]; then
     echo "__dune_env expected 1 argument, got $#"
     return
   fi
-  __dune_root="$1"
+  __DUNE_ROOT="$1"
 
-  # Add dune to PATH unless it's already present.
-  # Affix colons on either side of $PATH to simplify matching (based on
-  # rustup's env script).
-  case :"$PATH": in
-    *:"$__dune_root/bin":*)
-      # Do nothing since the bin directory is already in PATH.
+  case "$__DUNE_SETUP_STATE" in
+    incomplete)
+      # This is the first time __dune_env has been called, so attempt to set up
+      # the environment for dune and record in the global variable
+      # __DUNE_SETUP_STATE whether or not it succeeded.
+
+      # Add dune to PATH unless it's already present.
+      # Affix colons on either side of $PATH to simplify matching (based on
+      # rustup's env script).
+      case :"$PATH": in
+        *:"$__DUNE_ROOT/bin":*)
+          # Do nothing since the bin directory is already in PATH.
+          ;;
+        *)
+          export PATH="$__DUNE_ROOT/bin:$PATH"
+          ;;
+      esac
+
+      if [ "$(__dune_which)" = "$__DUNE_ROOT/bin/dune" ]; then
+        export __DUNE_SETUP_STATE=success
+      else
+        # Despite modifying the environment, running `dune` would resolve to
+        # the wrong dune instance. This can happen if the bin directory
+        # containing our dune was already present in PATH behind the bin
+        # directory of the default opam switch.
+        # TODO Possibly print a warning/hint in this case to help users fix
+        # their environment.
+        export __DUNE_SETUP_STATE=failure
+      fi
       ;;
-    *)
-      # Prepending path in case a system-installed dune needs to be overridden
-      export PATH="$__dune_root/bin:$PATH"
+    success)
+      # This is at least the second time this function was called in the
+      # current environment (possibly by the user sourcing their shell config
+      # or nesting shell sessions), and the previous time it was called it
+      # successfully modified the environment to give precedence to our dune
+      # executable. check that our dune still has precedence, and attempt to
+      # undo any opam-specific path shenanigans that have taken place since the
+      # last time this function was called.
+      if [ "$(__dune_which)" != "$__DUNE_ROOT/bin/dune" ]; then
+        case :"$PATH": in
+          *:"$__DUNE_ROOT/bin":*)
+            # Remove all opam bin directories from the PATH variable
+            # between the start of the PATH variable and the first
+            # occurrence of the dune binary distro's bin directory.
+            PATH_MAYBE_FIXED=$(echo "$PATH" | \
+              tr ':' '\n' |\
+              sed "1,\#^$__DUNE_ROOT/bin\$# { \#^$HOME/.opam#d; }" |\
+              paste -sd ':' -)
+            # Only commit the change if it actually fixed the problem.
+            if [ "$(PATH=$PATH_MAYBE_FIXED __dune_which)" = "$__DUNE_ROOT/bin/dune" ]; then
+              export PATH="$PATH_MAYBE_FIXED"
+            else
+              # The attempt to fix the PATH variable failed, so give up.
+              export __DUNE_SETUP_STATE=failure
+            fi
+            ;;
+          *)
+            # The dune binary distro is no longer in the PATH variable at
+            # all, so give up.
+            export __DUNE_SETUP_STATE=failure
+            ;;
+        esac
+      fi
+      ;;
+    failure)
+      # A previous attempt at modifying the environment failed, so don't
+      # attempt further environment modifications here.
       ;;
   esac
 }
